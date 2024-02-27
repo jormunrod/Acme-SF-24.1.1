ac60387e954a9415d77a1e2441b8b50010a62d82
alepez12 <alepersan3@alum.us.es>
2024-02-27 14:11:27 +0100

    [feat]: Code Audit entity implementation

---
+++ src/main/java/acme/entities/CodeAudit.java
@@ -0,0 +1,44 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class CodeAudit extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	@Past
+	private Date				execution;
+
+	private CodeAuditType		type;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				correctiveActions;
+
+	private Mark				mark;
+}
