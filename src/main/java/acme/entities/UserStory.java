f114e6ce3422ff3dc58a59a992aa3fa8ef13fe2c
Angelgares <angelgares6424@gmail.com>
2024-02-29 20:12:46 +0100

    [feat]: UserStory entity implementation

---
+++ src/main/java/acme/entities/UserStory.java
@@ -0,0 +1,55 @@
+
+package acme.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserStory extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@ManyToOne
+	private Project				project;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				description;
+
+	@NotNull
+	@Positive
+	private Integer				estimatedHours;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				acceptanceCriteria;
+
+	@NotNull
+	@Valid
+	private Priority			priority;
+
+	@URL
+	private String				link;
+
+}
