52e8a40153c28f23f77a262318a8b54b6f027d5d
IsaacSolisPadilla <isasolpad@alum.us.es>
2024-02-29 20:20:03 +0100

    [feat]: Developer entity

---
+++ src/main/java/acme/roles/Developer.java
@@ -0,0 +1,41 @@
+
+package acme.roles;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+public class Developer {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 76)
+	private String				degree;
+
+	@NotBlank
+	@Length(max = 101)
+	private String				specialisation;
+
+	@NotBlank
+	@Length(max = 101)
+	private String				skills;
+
+	@NotNull
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
