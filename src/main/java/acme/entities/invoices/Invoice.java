3c2e50d4fcbe8f8aaeb845b1e06599ac6f329ccf
JavierRodriguez <javrodrei@alum.us.es>
2024-03-02 12:01:15 +0100

    [feat]:Invoice entity

---
+++ src/main/java/acme/entities/invoices/Invoice.java
@@ -0,0 +1,61 @@
+
+package acme.entities.invoices;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Invoice extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
+	private String				code;
+
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationTime;
+
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				dueDate;
+
+	@NotNull
+	@Min(1)
+	private int					quantity;
+
+	@NotNull
+	@Min(1)
+	private int					tax;
+
+	@URL()
+	private String				link;
+
+
+	@Transient
+	public int getTotalAmount() {
+		return this.quantity + this.tax;
+	}
+}
