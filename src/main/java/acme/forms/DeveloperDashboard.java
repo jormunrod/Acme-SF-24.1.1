bd2cd956899a14c17c8826691b0eb6e9697bf810
IsaacSolisPadilla <isasolpad@alum.us.es>
2024-03-02 11:01:36 +0100

    [fix]: Dashboard name

--- src/main/java/acme/forms/DeveloperDashboard.java
+++ src/main/java/acme/forms/DeveloperDashboard.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class DeveloperDashboards extends AbstractForm {
+public class DeveloperDashboard extends AbstractForm {
 
 	// Serialisation identifier -----------------------------------------------
 
