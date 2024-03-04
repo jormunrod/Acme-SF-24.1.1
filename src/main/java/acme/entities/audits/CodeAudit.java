d819137e984dbac79139572272402507f975bf51
alepez12 <alepersan3@alum.us.es>
2024-03-04 15:31:18 +0100

    [fix]: CodeAudit attributes done

--- src/main/java/acme/entities/audits/CodeAudit.java
+++ src/main/java/acme/entities/audits/CodeAudit.java
@@ -5,6 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -15,6 +18,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
+import acme.entities.projects.Project;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -34,12 +38,11 @@ public class CodeAudit extends AbstractEntity {
 	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
 	private String				code;
 
-	//@Temporal(TemporalType.TIMESTAMP)
+	@Temporal(TemporalType.DATE)
 	@Past
 	@NotNull
 	private Date				execution;
 
-	@Valid
 	@NotNull
 	private CodeAuditType		type;
 
@@ -47,10 +50,19 @@ public class CodeAudit extends AbstractEntity {
 	@Length(max = 100)
 	private String				correctiveActions;
 
+	@NotNull
+	private Mark				mark;
+
 	@URL
+	@Length(max = 255)
 	private String				link;
 
 	// Derived attributes -----------------------------------------------------
 
-	//TODO: Attribute mark
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Project				project;
 }
