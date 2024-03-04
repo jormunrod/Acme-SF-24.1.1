5f6d5db748724da8ac1d18e45a631daf89cd62ea
Angelgares <angelgares6424@gmail.com>
2024-03-04 20:25:16 +0100

    [feat]: Objective & Project relation

--- src/main/java/acme/entities/projects/Objective.java
+++ src/main/java/acme/entities/projects/Objective.java
@@ -4,8 +4,10 @@ package acme.entities.projects;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -57,4 +59,9 @@ public class Objective extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Project				project;
 }
