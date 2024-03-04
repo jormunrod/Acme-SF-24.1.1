65f1d4a471797796e1e5a0830d34e3e4cd91d574
alepez12 <alepersan3@alum.us.es>
2024-03-04 15:58:59 +0100

    [fix]: AuditRecord attributes done.

--- src/main/java/acme/entities/audits/AuditRecord.java
+++ src/main/java/acme/entities/audits/AuditRecord.java
@@ -1,14 +1,21 @@
 
-package acme.entities;
+package acme.entities.audits;
 
-import java.time.Duration;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 import org.hibernate.validator.constraints.time.DurationMin;
 
 import acme.client.data.AbstractEntity;
@@ -31,11 +38,24 @@ public class AuditRecord extends AbstractEntity {
 	@Pattern(regexp = "AU-[0-9]{4}-[0-9]{3}")
 	private String				code;
 
+	@Temporal(TemporalType.TIMESTAMP)
 	@Past
 	@DurationMin(hours = 1)
-	private Duration			period;
+	private Date				period;
 
+	@NotNull
 	private Mark				mark;
 
+	@URL
+	@Length(max = 255)
 	private String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private CodeAudit			codeAudit;
 }
