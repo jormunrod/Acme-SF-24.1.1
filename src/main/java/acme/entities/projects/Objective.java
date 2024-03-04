7c1e158aedc1f5c0b046cdc493e7e32c8b29db14
Angelgares <angelgares6424@gmail.com>
2024-03-04 17:43:35 +0100

    [feat]: Objective entity implementation

---
+++ src/main/java/acme/entities/projects/Objective.java
@@ -0,0 +1,60 @@
+
+package acme.entities.projects;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Objective extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Past
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				instantationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				description;
+
+	@NotNull
+	private ObjectivePriority	priority;
+
+	private boolean				isCritical;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				duration;
+
+	@URL
+	@Length(max = 255)
+	private String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+}
