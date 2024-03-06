f17d10981adba4d947cb468216af954de5dbebfe
IsaacSolisPadilla <isasolpad@alum.us.es>
2024-03-06 20:37:48 +0100

    [fix]: Delete a restricion

--- src/main/java/acme/entities/trainings/TrainingModule.java
+++ src/main/java/acme/entities/trainings/TrainingModule.java
@@ -59,7 +59,6 @@ public class TrainingModule extends AbstractEntity {
 	@Length(max = 255)
 	private String				link;
 
-	@NotNull
 	@Positive
 	private int					totalTime;
 	// Derived attributes -----------------------------------------------------
