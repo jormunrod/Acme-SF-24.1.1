e048d59a6fd0031ee4c3f69cafeb8c1ab03c81ac
Angelgares <angelgares6424@gmail.com>
2024-03-07 00:37:20 +0100

    [fix]: UserStory entity relations

--- src/main/java/acme/entities/projects/UserStory.java
+++ src/main/java/acme/entities/projects/UserStory.java
@@ -2,8 +2,6 @@
 package acme.entities.projects;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -51,10 +49,5 @@ public class UserStory extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-
-	@NotNull
-	@Valid
-	@ManyToOne(optional = false)
-	private Project				project;
 
 }
