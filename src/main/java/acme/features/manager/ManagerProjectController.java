253fbcc032e1680ab4cea4caac39616669af6a6d
Angelgares <angelgares6424@gmail.com>
2024-04-10 13:13:04 +0200

    [feat]: Project list implementation

---
+++ src/main/java/acme/features/manager/ManagerProjectController.java
@@ -0,0 +1,28 @@
+
+package acme.features.manager;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.client.controllers.AbstractController;
+import acme.entities.projects.Project;
+import acme.roles.Manager;
+
+@Controller
+public class ManagerProjectController extends AbstractController<Manager, Project> {
+
+	@Autowired
+	private ManagerProjectListService	listService;
+
+	@Autowired
+	private ManagerProjectShowService	showService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+		super.addBasicCommand("show", this.showService);
+	}
+}
