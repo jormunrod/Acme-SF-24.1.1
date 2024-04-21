e69967a5902989b88f3da4abf1dcf9cc5272f7a4
Alejandro Pérez <alepersan3@alum.us.es>
2024-04-22 00:56:34 +0200

    [WIP]: Auditor Dashboard repository

--- src/main/java/acme/features/auditor/dashboard/AuditorDashboardRepository.java
+++ src/main/java/acme/features/auditor/dashboard/AuditorDashboardRepository.java
@@ -15,12 +15,15 @@ public interface AuditorDashboardRepository extends AbstractRepository {
 	@Query("select count(ca) from CodeAudit ar where ca.type = 'DYNAMIC'")
 	Integer totalNumberOfDynamicCodeAudits();
 
+	@Query("select avg(select count(ar) from AuditRecord ar where a.worker.id = w.id) from CodeAudit ca")
 	Double averageNumberOfAuditRecord();
 
-	Double deviationNumberOfAuditRecord();
+	//Double deviationNumberOfAuditRecord();
 
+	@Query("select min(select count(ar) from AuditRecord ar where a.worker.id = w.id) from CodeAudit ca")
 	Integer minimumNumberOfAuditRecord();
 
+	@Query("select max(select count(ar) from AuditRecord ar where a.worker.id = w.id) from CodeAudit ca")
 	Integer maximumNumberOfAuditRecord();
 
 	Double averageTimeOfPeriodInAuditRecord();
